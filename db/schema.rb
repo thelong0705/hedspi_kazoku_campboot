4e7ba0e94d9cfab75fb10538721d0dcf09ef6646
thm1707 <tranhoangminh1707@gmail.com>
2018-10-05 21:55:59 +0700

    change sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_145922) do
+ActiveRecord::Schema.define(version: 2018_10_04_145923) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2018_10_04_145922) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "picture"
-    t.string "gender"
+    t.integer "gender"
     t.string "company"
     t.integer "school_year"
     t.index ["email"], name: "index_users_on_email", unique: true
