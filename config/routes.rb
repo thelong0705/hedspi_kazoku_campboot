5cf0e7d0d7d671a729f628cf9c1b7fe2bdfb5a80
myroad2pro <hoanganha91013@gmail.com>
2018-10-08 07:47:58 +0000

    Add user profile view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users, :only => [:show]
 end
