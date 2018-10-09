f86f9e874acb835ac13a11bda3446b8a6e7011e5
thănglongsp <thanglongnguyensp@gmail.com>
2018-10-09 05:38:05 +0000

    completed validation login and signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable 
-  validates :email, :password, :name, :company, :gender, :school_year,  presence: true
+  validates :email, :password, :name, :company, :gender, :password_confirmation, presence: true
+  validates :school_year, inclusion: { in: 1..5 }
 end
