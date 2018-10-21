5563166295fbbe590a28c99a1f967dfa3dcb97c3
thănglongsp <thanglongnguyensp@gmail.com>
2018-10-21 03:13:45 +0000

    en <> vi ver 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  resources :companies
-  root 'static_pages#home'
-
-  devise_for :users,
-             :skip => [:registrations]
-
-  devise_scope :user do
-    get "users/sign_up", to: "users/registrations#new", as: :new_user_registration
-    post "users/sign_up", to: "users/registrations#create", as: :user_registration
-    get "users/edit", to: "users/registrations#edit", as: :edit_user
-    put "users/sign_up", to: "users/registrations#update"
-  end
+  scope "(:locale)", locale: /en|vi/ do
+    resources :companies
+    root 'static_pages#home'
   
-  resources :users, :only => [:show]
+    devise_for :users,
+               :skip => [:registrations]
+  
+    devise_scope :user do
+      get "users/sign_up", to: "users/registrations#new", as: :new_user_registration
+      post "users/sign_up", to: "users/registrations#create", as: :user_registration
+      get "users/edit", to: "users/registrations#edit", as: :edit_user
+      put "users/sign_up", to: "users/registrations#update"
+    end
+    
+    resources :users, :only => [:show]
+  end
 end
