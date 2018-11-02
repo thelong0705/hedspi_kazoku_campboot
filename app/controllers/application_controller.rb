191fde1576d6094914dc3c86baccc4cc5880e753
thănglongsp <thanglongnguyensp@gmail.com>
2018-11-02 16:51:20 +0000

    en<>vi for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :set_locale
+   
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+  
+  def extract_locale_from_tld
+    parsed_locale = request.host.split('.').last
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+  
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :admins, skip: [:registrations], controllers: { sessions: "admins/sessions"}
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-
-  resources :companies do
-    member do
-      put 'like' => 'companies#like'
-    end
-    resources :comments
-  end
-  root 'static_pages#home'
-
-  devise_for :users,
-             skip: [:registrations], controllers: { sessions: "users/sessions" }
-
-  devise_scope :user do
-    get 'users/sign_up', to: 'users/registrations#new', as: :new_user_registration
-    post 'users/sign_up', to: 'users/registrations#create', as: :user_registration
-    get 'users/edit', to: 'users/registrations#edit', as: :edit_user
-    put 'users/sign_up', to: 'users/registrations#update'
-  end
+  scope "(:locale)", locale: /en|vi/ do
+    devise_for :admins, skip: [:registrations], controllers: { sessions: "admins/sessions"}
+    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
-  resources :users, only: [:show]
+    resources :companies do
+      member do
+        put 'like' => 'companies#like'
+      end
+      resources :comments
+    end
+    root 'static_pages#home'
+  
+    devise_for :users,
+               skip: [:registrations], controllers: { sessions: "users/sessions" }
+  
+    devise_scope :user do
+      get 'users/sign_up', to: 'users/registrations#new', as: :new_user_registration
+      post 'users/sign_up', to: 'users/registrations#create', as: :user_registration
+      get 'users/edit', to: 'users/registrations#edit', as: :edit_user
+      put 'users/sign_up', to: 'users/registrations#update'
+    end
+    
+    resources :users, only: [:show]
+  end
 end
