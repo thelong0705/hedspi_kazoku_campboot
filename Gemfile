9c60b992b77d71f560ce5873c925f67248d949e5
Cao Văn Minh <minhcvbachkhoa@gmail.com>
2018-12-04 02:09:11 +0000

    Chat with realtime

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem 'ratyrate'
 gem 'devise'
 gem 'bootstrap-will_paginate'
 
+gem 'redis', '~> 3.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,13 @@ class MessagesController < ApplicationController
   def create
     @conversation = Conversation.includes(:recipient).find(params[:conversation_id])
     @message = @conversation.messages.create(message_params)
+    if @message
+      ActionCable.server.broadcast 'messages',
+        message: @message.body,
+        user: @message.user,
+        conversation: @message.conversation
+      head :ok
+    end
  
     respond_to do |format|
       format.js
